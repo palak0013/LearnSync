96f8223ab64cbe2343d9e713e7ae9c65356a2bc0
Palak Singh <2023baiml034@axiscolleges.in>
2026-07-28 11:37:44 +0530

    Completed the learning spaces CRUD

--- api/space.py
+++ api/space.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from database.database import get_db
 from api.auth import get_current_user
-from crud.space import create_space
+from crud.space import create_space, update_space, delete_space
+from crud.space import get_spaces, get_space_by_id
 from schemas.space import (SpaceCreate, SpaceResponse,)
 from models.user import User
 
@@ -25,5 +26,69 @@ def create_new_space(
     return create_space(
         db=db,
         space=space,
+        current_user=current_user
+    )
+    
+
+@router.get(
+    "",
+    response_model=list[SpaceResponse]
+)
+def get_all_spaces(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    return get_spaces(
+        db=db,
+        current_user=current_user
+    )
+    
+
+@router.get(
+    "/{space_id}",
+    response_model=SpaceResponse
+)
+def get_space(
+    space_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+
+    return get_space_by_id(
+        db=db,
+        space_id=space_id,
+        current_user=current_user
+    )
+    
+
+@router.put(
+    "/{space_id}",
+    response_model=SpaceResponse
+)
+def update_space_api(
+    space_id: int,
+    space: SpaceCreate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+
+    return update_space(
+        db=db,
+        space_id=space_id,
+        space=space,
+        current_user=current_user
+    )
+    
+    
+@router.delete("/{space_id}")
+def delete_space_api(
+    space_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+
+    return delete_space(
+        db=db,
+        space_id=space_id,
         current_user=current_user
     )
--- crud/space.py
+++ crud/space.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-
+from fastapi import HTTPException
 from models.space import Space
 from models.user import User
 from schemas.space import SpaceCreate
@@ -21,3 +21,77 @@ def create_space(
     db.refresh(new_space)
 
     return new_space
+
+def get_spaces(
+    db: Session,
+    current_user: User,
+):
+    return(
+        db.query(Space).filter(Space.owner_id == current_user.id).all()
+    )
+    
+def get_space_by_id(
+    db: Session,
+    space_id: int,
+    current_user: User,
+):
+    space = (
+    db.query(Space).filter(Space.id == space_id, Space.owner_id == current_user.id).first()
+    )
+    if space is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Space not found"
+        )
+    return space
+
+
+def update_space(
+    db: Session,
+    space_id: int,
+    space: SpaceCreate,
+    current_user: User,
+):
+    existing_space = (
+        db.query(Space).filter(Space.id == space_id, Space.owner_id == current_user.id).first()
+    )
+
+    if existing_space is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Space not found"
+        )
+
+    existing_space.name = space.name
+    existing_space.description = space.description
+
+    db.commit()
+    db.refresh(existing_space)
+
+    return existing_space
+
+
+def delete_space(
+    db: Session,
+    space_id: int,
+    current_user: User,
+):
+    space = (
+        db.query(Space)
+        .filter(
+            Space.id == space_id,
+            Space.owner_id == current_user.id
+        )
+        .first()
+    )
+
+    if space is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Space not found"
+        )
+
+    db.delete(space)
+    db.commit()
+
+    return {"message": "Space deleted successfully"}
